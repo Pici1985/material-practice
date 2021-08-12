3145ec080256588407f821ef419eb9002fb1d949
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2021-08-12 06:34:22 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material/material.module';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { ButtonToggleComponent } from './button-toggle/button-toggle.component';
+import { IconsComponent } from './icons/icons.component';
+import { BadgesComponent } from './badges/badges.component';
+import { ProgressSpinnerComponent } from './progress-spinner/progress-spinner.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MenusComponent } from './menus/menus.component';
+import { ListComponent } from './list/list.component';
+import { GridListComponent } from './grid-list/grid-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent,
+    ButtonToggleComponent,
+    IconsComponent,
+    BadgesComponent,
+    ProgressSpinnerComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    MenusComponent,
+    ListComponent,
+    GridListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
